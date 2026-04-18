6993a0aba8610d7614b6a7cc5e2e9bf12dd55723
Brenda Luana <brendalfcastro@gmail.com>
2026-04-18 13:53:04 -0300

    Ciclo 2 testes GREEN

--- gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/repositories/VeterinarioRepository.java
+++ gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/repositories/VeterinarioRepository.java
@@ -16,6 +16,13 @@ public interface VeterinarioRepository extends JpaRepository<Veterinario, Intege
     List<Veterinario> findByNome(String nome); // exato (case sensitive)
     List<Veterinario> findByNomeIgnoreCase(String nome);// case insensitive aqui pega tanto maiusculo quanto minusculo
 
+   //Ciclo 2 - buscando por pedaço de nome usando o LIKE
+   //  List<Veterinario> findByNomeContainsIgnoreCase(String nome);  //no JPA o contains já entende o LIKE, nesse caso ignora se é maíusculo ou minisculo
+
+   // mas vou usar o like conforme o exercicio fala
+
+   List<Veterinario> findByNomeLike(String nome);
+   List<Veterinario> findByNomeLikeIgnoreCase(String nome);
 
 }
 
